d3a71b2591e54f0609f7b0b994664f667f1568b4
Asyraq <asyraq@MD1124-016.local>
2026-05-14 16:47:45 +0800

    Fix white screen by importing useEffect in PublicFlow

--- frontend/src/PublicFlow.jsx
+++ frontend/src/PublicFlow.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Landing from './components/Landing'
 import NameSelection from './components/NameSelection'
 import GenderSelection from './components/GenderSelection'
